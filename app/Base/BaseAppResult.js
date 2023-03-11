852dd00ed292fff387c9c067e8ea14d034cec806
aliNejati-bu <electrocellco@gmail.com>
2023-03-11 16:33:22 +0330

    Add Base App Result.

---
+++ app/Base/BaseAppResult.js
@@ -0,0 +1,9 @@
+module.exports = class {
+    constructor(
+        status,
+        statusCode
+    ) {
+        this.status = status;
+        this.statusCode = statusCode;
+    }
+}
---
+++ app/Base/BaseAppStatusCode.js
@@ -0,0 +1,4 @@
+module.exports = {
+    NotFounded: 'NotFounded',
+    Unknown: 'Unknown'
+}
