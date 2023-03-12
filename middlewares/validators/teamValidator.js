9ba129b8c280171eddf179ee7bdb0e3e198aba10
aliNejati-bu <electrocellco@gmail.com>
2023-03-12 07:58:04 +0330

    add AddMember in router layer.

--- middlewares/validators/teamValidator.js
+++ middlewares/validators/teamValidator.js
@@ -4,3 +4,7 @@ const Joi = require('joi');
 module.exports.create = Joi.object().keys({
     name: Joi.string().required()
 });
+
+module.exports.addMember = Joi.object().keys({
+    userEmail: Joi.string().email().required()
+});
--- router/team.js
+++ router/team.js
@@ -1,15 +1,15 @@
 const Router = require("express").Router();
 const wrapMiddleware = require('../middlewares/wrapMidlleware');
-const userValidator = require('../middlewares/validators/userValidator')
 const {baseResponse} = require('../utils/functions');
 const {resultCode} = require('../utils/works');
 const verifyTokenMiddleware = require('../middlewares/verifyTokenMiddleware');
-const teamMiddleware = require('../middlewares/validators/teamValidator');
+const teamValidator = require('../middlewares/validators/teamValidator');
 const _ = require('lodash');
 const {teamApp} = require('../instances/app');
+const canAddMember = require('../middlewares/canAddMember');
+const BaseAppStatusCode = require('../app/Base/BaseAppStatusCode');
 
-
-Router.post('/', wrapMiddleware(teamMiddleware.create),verifyTokenMiddleware, async (req, res) => {
+Router.post('/', wrapMiddleware(teamValidator.create), verifyTokenMiddleware, async (req, res) => {
     try {
         let result = await teamApp.createTeam(req.body.token.id, req.body.name);
         if (!result) {
@@ -21,5 +21,25 @@ Router.post('/', wrapMiddleware(teamMiddleware.create),verifyTokenMiddleware, as
         return baseResponse(res, false, 'server error.', resultCode.ServerError,);
     }
 })
+
+
+Router.post('/:tid/addMember', wrapMiddleware(teamValidator.addMember), verifyTokenMiddleware, canAddMember, async (req, res) => {
+    const result = await teamApp.addUserToTeam(req.body.userEmail, req.params.tid);
+
+    if (!result.status) {
+        if (result.statusCode === BaseAppStatusCode.Duplicate) {
+            return baseResponse(res, false, {}, resultCode.Duplicate, 409);
+        } else if (result.statusCode === BaseAppStatusCode.NotFounded) {
+            return baseResponse(res, false, {}, resultCode.NotFounded, 404);
+        } else {
+            return baseResponse(res, false, {}, resultCode.ServerError, 500);
+        }
+    }
+
+    return baseResponse(res, true, {
+        message: 'member added',
+    });
+});
+
 
 module.exports = Router;
